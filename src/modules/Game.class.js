79eb46e43f2bfe0f818f7f00fc074c12db3ed060
Owk <kkoctuk2005@gmail.com>
2025-04-29 13:41:48 +0300

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,64 +5,286 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
+const randomNum = (minimun, maximum) => {
+  const min = Math.ceil(minimun);
+  const max = Math.floor(maximum);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const twoOrFour = () => {
+  if (Math.random() < 0.9) {
+    return 2;
+  } else {
+    return 4;
+  }
+};
+
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.size = 4;
+    this.score = 0;
+    this.state = 'idle';
+    this.board = initialState || this.createBoard();
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  createBoard() {
+    const board = [];
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+    for (let i = 0; i < this.size; i++) {
+      const row = [];
 
-  /**
-   * @returns {number[][]}
-   */
+      for (let j = 0; j < this.size; j++) {
+        row.push(0);
+      }
+
+      board.push(row);
+    }
+
+    return board;
+  }
+
+  updateBoard(rows) {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        const value = this.board[i][j];
+
+        rows[i].cells[j].innerText = value;
+
+        if (value === 0) {
+          rows[i].cells[j].innerText = '';
+        }
+      }
+    }
+  }
+
+  checkForLose() {
+    const board = this.board;
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        const current = board[i][j];
+
+        if (current === 0) {
+          return false;
+        }
+
+        if (j < 3 && current === board[i][j + 1]) {
+          return false;
+        }
+
+        if (i < 3 && current === board[i + 1][j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  generate() {
+    const board = this.board;
+    const emptyCells = [];
+
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === 0) {
+          emptyCells.push([i, j]);
+        }
+      }
+    }
+
+    if (emptyCells.length > 0) {
+      const position = randomNum(0, emptyCells.length);
+      const [x, y] = emptyCells[position];
+
+      board[x][y] = twoOrFour();
+    }
+  }
+
+  combineHorizontally(left, right) {
+    const board = this.board;
+
+    for (let i = 0; i < board.length; i++) {
+      const row = board[i];
+
+      if (left) {
+        for (let j = 0; j < row.length - 1; j++) {
+          if (row[j] !== 0 && row[j] === row[j + 1]) {
+            const combined = row[j] + row[j + 1];
+
+            row[j] = combined;
+            row[j + 1] = 0;
+          }
+        }
+      }
+
+      if (right) {
+        for (let j = row.length - 1; j > 0; j--) {
+          if (row[j] !== 0 && row[j] === row[j - 1]) {
+            const combined = row[j] + row[j - 1];
+
+            row[j] = combined;
+            row[j - 1] = 0;
+          }
+        }
+      }
+    }
+
+    return board;
+  }
+
+  combineVertically(up, down) {
+    const board = this.board;
+    const columns = [];
+
+    for (let i = 0; i < 4; i++) {
+      const column = [];
+
+      for (let j = 0; j < 4; j++) {
+        column.push(board[j][i]);
+      }
+
+      columns.push(column);
+    }
+
+    for (let i = 0; i < columns.length; i++) {
+      const column = columns[i];
+
+      if (up) {
+        for (let j = 0; j < column.length - 1; j++) {
+          if (column[j] !== 0 && column[j] === column[j + 1]) {
+            const combined = column[j] + column[j + 1];
+
+            column[j] = combined;
+            column[j + 1] = 0;
+          }
+        }
+      }
+
+      if (down) {
+        for (let j = column.length - 1; j > 0; j--) {
+          if (column[j] !== 0 && column[j] === column[j - 1]) {
+            const combined = column[j] + column[j - 1];
+
+            column[j] = combined;
+            column[j - 1] = 0;
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        board[i][j] = columns[j][i];
+      }
+    }
+  }
+
+  moveHorizontally(left, right) {
+    const board = this.board;
+
+    for (let i = 0; i < board.length; i++) {
+      const row = board[i];
+      const filtered = row.filter((value) => value !== 0);
+
+      if (left) {
+        while (filtered.length < row.length) {
+          filtered.push(0);
+        }
+      }
+
+      if (right) {
+        while (filtered.length < row.length) {
+          filtered.unshift(0);
+        }
+      }
+
+      board[i] = filtered;
+    }
+
+    return board;
+  }
+
+  moveVertically(up, down) {
+    const board = this.board;
+    const columns = [];
+
+    for (let i = 0; i < 4; i++) {
+      const column = [];
+
+      for (let j = 0; j < 4; j++) {
+        column.push(board[j][i]);
+      }
+
+      columns.push(column);
+    }
+
+    for (let k = 0; k < columns.length; k++) {
+      const column = columns[k];
+      const filtered = column.filter((value) => value !== 0);
+
+      while (filtered.length < column.length) {
+        if (up) {
+          filtered.push(0);
+        } else if (down) {
+          filtered.unshift(0);
+        }
+      }
+
+      columns[k] = filtered;
+    }
+
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        board[i][j] = columns[j][i];
+      }
+    }
+  }
+
+  getScore(cells) {
+    this.score = cells.reduce((acc, curr) => {
+      const num = parseInt(curr.innerText);
+
+      return acc + (isNaN(num) ? 0 : num);
+    }, 0);
+
+    return this.score;
+  }
+
   getState() {}
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
   getStatus() {}
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start() {
+    this.state = 'playing';
+    this.score = 0;
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+    const x1 = randomNum(0, 3);
+    let x2 = randomNum(0, 3);
+    const y1 = randomNum(0, 3);
+    let y2 = randomNum(0, 3);
 
-  // Add your own methods here
+    while (x1 === x2 && y1 === y2) {
+      x2 = randomNum(0, 3);
+      y2 = randomNum(0, 3);
+    }
+
+    this.board[y1][x1] = twoOrFour();
+    this.board[y2][x2] = twoOrFour();
+  }
+
+  restart() {
+    this.score = 0;
+    this.state = 'idle';
+
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,130 @@
-'use strict';
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const gameField = document.querySelector('.game-field');
+const rows = Array.from(gameField.rows);
+const cells = rows.map((row) => Array.from(row.cells)).flat();
+const scoreTab = document.querySelector('.game-score');
+const winMessage = document.querySelector('.message.message-win.hidden');
+const loseMessage = document.querySelector('.message.message-lose.hidden');
+const startMessage = document.querySelector('.message.message-start');
 
-// Write your code here
+const updateScore = () => {
+  game.getScore(cells);
+  scoreTab.innerText = game.score;
+};
+
+const checkForWin = () => {
+  if (scoreTab.innerText === '2048') {
+    winMessage.classList.remove('hidden');
+  }
+};
+
+const checkForLose = () => {
+  if (game.checkForLose()) {
+    loseMessage.classList.remove('hidden');
+  }
+};
+
+document.addEventListener('click', (e) => {
+  const start = e.target.closest('.button.start');
+  const restart = e.target.closest('.button.restart');
+
+  if (start) {
+    start.classList.remove('start');
+    start.classList.add('restart');
+    start.innerText = 'Restart';
+    game.start();
+    game.updateBoard(rows);
+    updateScore();
+    addColors();
+
+    startMessage.classList.add('hidden');
+  }
+
+  if (restart) {
+    restart.classList.remove('restart');
+    restart.classList.add('start');
+    restart.innerText = 'Start';
+    game.restart();
+    game.updateBoard(rows);
+    updateScore();
+    addColors();
+
+    if (!winMessage.classList.contains('hidden')) {
+      winMessage.classList.add('hidden');
+    }
+
+    if (!loseMessage.classList.contains('hidden')) {
+      loseMessage.classList.add('hidden');
+    }
+
+    if (startMessage.classList.contains('hidden')) {
+      startMessage.classList.remove('hidden');
+    }
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowUp') {
+    game.moveVertically(true, false);
+    game.combineVertically(true, false);
+    game.moveVertically(true, false);
+    game.generate();
+    game.updateBoard(rows);
+    updateScore();
+    addColors();
+    checkForWin();
+    checkForLose();
+  }
+
+  if (e.key === 'ArrowDown') {
+    game.moveVertically(false, true);
+    game.combineVertically(false, true);
+    game.moveVertically(false, true);
+    game.generate();
+    game.updateBoard(rows);
+    updateScore();
+    addColors();
+    checkForWin();
+    checkForLose();
+  }
+
+  if (e.key === 'ArrowLeft') {
+    game.moveHorizontally(true, false);
+    game.combineHorizontally(true, false);
+    game.moveHorizontally(true, false);
+    game.generate();
+    game.updateBoard(rows);
+    updateScore();
+    addColors();
+    checkForWin();
+    checkForLose();
+  }
+
+  if (e.key === 'ArrowRight') {
+    game.moveHorizontally(false, true);
+    game.combineHorizontally(false, true);
+    game.moveHorizontally(false, true);
+    game.generate();
+    game.updateBoard(rows);
+    updateScore();
+    addColors();
+    checkForWin();
+    checkForLose();
+  }
+});
+
+function addColors() {
+  for (let i = 0; i < cells.length; i++) {
+    for (const className of cells[i].classList) {
+      if (className.startsWith('field-cell--')) {
+        cells[i].classList.remove(className);
+      }
+    }
+
+    if (parseInt(cells[i].innerText) > 0) {
+      cells[i].classList.add(`field-cell--${parseInt(cells[i].innerText)}`);
+    }
+  }
+}
